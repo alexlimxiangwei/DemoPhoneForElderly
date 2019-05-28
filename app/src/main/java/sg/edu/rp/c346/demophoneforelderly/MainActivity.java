b2de04f1a6fafee28b010639ffe24af98c000847
alexlimxiangwei <alex.lim.xiang.wei@gmail.com>
2019-05-28 11:29:19 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/demophoneforelderly/MainActivity.java
@@ -0,0 +1,38 @@
+package sg.edu.rp.c346.demophoneforelderly;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnSon;
+    Button btnDau;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnSon = findViewById(R.id.btnCallSon);
+        btnDau = findViewById(R.id.btnCallDaughter);
+
+        btnSon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentCall = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:"+92240336));
+                startActivity(intentCall);
+            }
+        });
+        btnDau.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentCall = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:"+89675543));
+                startActivity(intentCall);
+            }
+        });
+    }
+}
